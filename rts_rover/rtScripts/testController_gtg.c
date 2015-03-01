c436f20443f4b580d70e693b9a81af4ba74a8afd
root <root@beaglebone.localdomain>
2015-03-01 22:49:39 +0000

    testController added

--- rts_rover/rtScripts/testController_gtg.c
+++ rts_rover/rtScripts/testController_gtg.c
@@ -14,6 +14,7 @@ Encoder r - A: P8_11
 Encoder l - A: P8_15
 	    B: P8_16
 */
+
 #include <fcntl.h>
 
 #define HIGH 1
@@ -38,14 +39,14 @@ Encoder l - A: P8_15
 #define GPIO2_ADDR 0x481AC000
 #define GPIO3_ADDR 0x481AF000
 
-#define period1 1000000//800000
-#define period2 1000000//800000
-#define period_gtg 1000000
+#define period_motor 2000000 //2 ms
+#define period_encoder 10000000 //10 ms
+#define period_gtg 10000000 //10 ms
+#define period_odo 10000000 //10 ms
+#define period_testGoal 5000000 // 5ms
 
-#define duty1 period1*0.25
-#define duty2 period2*0.25
-
-#define period 100000
+#define dutyL period_motor*.35
+#define dutyR period_motor*.35
 
 #define MAX_BUF 64
 
@@ -57,6 +58,7 @@ RT_TASK lMotor_task;
 RT_TASK stop_rMotor;
 RT_TASK stop_lMotor;
 RT_TASK gtg_task;
+RT_TASK testGoal_task;
 
 RT_SEM rSem;
 RT_SEM lSem;
@@ -80,20 +82,52 @@ double pwm_l = 0;
 #define L .185
 #define N 1000/3 //Ticks per revolution
 #define pi 3.14159
-#define m_per_tick 2*pi*R/N
-#define v 0.15 //in m/s
-#define Kp 1
+#define m_per_tick 2*pi*R/N //5.654e-4
+#define v 0.15 //max is 6.66
+#define Kp 4
 #define Ki 0
 #define Kd 0.01
-#define max_duty 0.75
-#define max_vel 0.18
-#define w_max 0.20 //in rad/sec
-#define w_min -0.20 
+#define max_duty 0.35
+#define min_duty 0
+
+#define x_min 0.001
+#define y_min 0.001
+
+//maximum velocities
+#define max_rpm 200 //130
+#define min_rpm 30
+#define max_vel  (max_rpm*pi)/60
+#define min_vel  (min_rpm*pi)/60
 
 int map(double);
 
-const int x_g = 1;
-const int y_g = 1;
+// Define Goal position
+const double x_g = .1;
+const double y_g = .1;
+
+void testGoal(void *arg){
+	double error_pos_x, error_pos_y;
+        RTIME prev, now, dt;
+        RTIME previous, now_wcet;
+        long MAX = 0;
+        int flag = 1;
+
+        rt_task_set_periodic(NULL, TM_NOW, period_testGoal); 
+        prev = rt_timer_read(); 
+
+        while(1){
+                rt_task_wait_period(NULL);
+                previous = rt_timer_read();
+		error_pos_x = x_g - x;
+		error_pos_y = y_g -y;
+		rt_printf("Error in x and y are: %lf %lf \n", error_pos_x, error_pos_y);
+
+		if(((-x_min < error_pos_x) && (error_pos_x < x_min)) && ((error_pos_y < y_min) && (error_pos_y > -y_min))){
+			rt_printf("At a goal");
+			cleanup();
+		}
+}
+}
 
 void gtg(void *arg){
 
@@ -108,13 +142,16 @@ void gtg(void *arg){
 	double vel_r, vel_l;
 
 	RTIME prev, now, dt;
+	RTIME previous, now_wcet;
+	long MAX = 0;
+        int flag = 1;
 
 	rt_task_set_periodic(NULL, TM_NOW, period_gtg); 
 	prev = rt_timer_read();	
 
 	while(1){
 		rt_task_wait_period(NULL);
-
+		previous = rt_timer_read();
 		u_x = x_g - x;
 		u_y = y_g - y; 
 		theta_g = atan2(u_y, u_x);
@@ -135,29 +172,54 @@ void gtg(void *arg){
 		w = Kp*e_P + Ki*e_I + Kd*e_D;
 		E_k = e_I;
 		e_k_1 = e_k;
-//		rt_printf("v and w are: %lf, %lf \n", v, w);
-		
-		if (w > w_max){
-			w = w_max;
-		}else if(w < w_min){
-			w = w_min;
-		}	
-                rt_printf("v and w are: %lf, %lf \n", v, w);
+
+//               rt_printf("v and w are: %lf, %lf \n", v, w);
 
 		//uni to diff
 		vel_r = (2*v + w*L)/(2*R);
 		vel_l = (2*v - w*L)/(2*R);
-		rt_printf("vel_r and vel_l are: %lf, %lf \n", vel_r, vel_l);
+//		rt_printf("vel_r and vel_l are: %lf, %lf \n", vel_r, vel_l);
+	
+		//limit vel_r and vel_l
+/*		if (vel_r > max_vel){
+			vel_r = max_vel;
+		}
+
+		if (vel_r < min_vel){
+			vel_r = min_vel;
+		}
+                if (vel_l > max_vel){
+                        vel_l = max_vel;
+                }
+
+                if (vel_l < min_vel){
+                        vel_l = min_vel;
+                }
+                rt_printf("vel_r and vel_l are: %lf, %lf \n", vel_r, vel_l);
+
+		vel_l = abs(vel_l);
+		vel_r = abs(vel_r);
+*/
 		//get pwm values input:0-v and output 0-.75% 
 		rt_sem_p(&rPwm, 0);
-		pwm_r =  (max_duty/max_vel)*vel_r;
+		pwm_r =  min_duty + ((max_duty - min_duty)/(max_vel - min_vel))*(vel_r - min_vel);
 		rt_sem_v(&rPwm);
 
 		rt_sem_p(&lPwm, 0);
-		pwm_l = (max_duty/max_vel)*vel_l;
+                pwm_l =  min_duty + ((max_duty - min_duty)/(max_vel - min_vel))*(vel_l - min_vel);
 		rt_sem_v(&lPwm);
 
 		rt_printf("pwmr: %lf, pwml: %lf \n", pwm_r, pwm_l);
+               	now_wcet = rt_timer_read();
+                if (flag){
+                        MAX = now_wcet - previous;
+                        flag = 0;
+                }
+
+                if((long)((now_wcet - previous)) > MAX){
+                        MAX = (long)((now_wcet - previous)) ;
+                }
+//                rt_printf("WCET Go-to-goal : %ld \n", MAX);
 		
 	}
 }
@@ -194,6 +256,7 @@ void rMotor(void *arg)
         RT_TASK_INFO curtaskinfo;
 	RTIME now, previous;
 	long MAX = 0;
+	int flag = 1;
 
 	int fd = open("/dev/mem",O_RDWR | O_SYNC);
     	ulong* pinconf1 =  (ulong*) mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO1_ADDR);	
@@ -201,67 +264,78 @@ void rMotor(void *arg)
 	//configure logic pins
      	pinconf1[GPIO_DATAOUT/4]  |= (1 << 28); //set pin  P9_12
      	pinconf1[GPIO_DATAOUT/4]  &= ~(1 << 17); // clear pin P9_15
-	rt_task_set_periodic(NULL, TM_NOW, period1);
+	rt_task_set_periodic(NULL, TM_NOW, period_motor);
         rt_printf("Controling Right Motors!\n");
-//	previous = rt_timer_read();
+
 	while (1){
                 rt_task_wait_period(NULL);
 		previous = rt_timer_read();
 		pinconf1[GPIO_DATAOUT/4] |= (1 << 17); //PWM on pin P9_23
-		rt_task_sleep(duty1);
-		pinconf1[GPIO_DATAOUT/4] &= ~(1 << 17); //toggle pin
-//		rt_printf("right Motor PWM, time taken:%ld. %06ld ms\n", (long)(now-previous)/1000000, (long)(now-previous)%1000000);
+		rt_task_sleep(pwm_r*period_motor);
+//		rt_task_sleep(dutyR);
+		pinconf1[GPIO_DATAOUT/4] &= ~(1 << 17); 
 		now = rt_timer_read();
-		if((long)((now - previous)%1000000) > MAX){
-			MAX = (long)((now - previous)%1000000) ;
+		if (flag){
+			MAX = now- previous;
+			flag = 0;
+		}
+
+		if((long)((now - previous)) > MAX){
+			MAX = (long)((now - previous)) ;
 		}
 //		rt_printf("WCET Right Motor: %ld \n", MAX);
-		//previous = now;
 	}
 }
 
 void lMotor(void *arg)
 {
-        RT_TASK *curtask;
-        RT_TASK_INFO curtaskinfo;
         RTIME now, previous;
 	long MAX = 0;
+	int flag = 1;
+
         int fd = open("/dev/mem",O_RDWR | O_SYNC);
         ulong* pinconf2 =  (ulong*) mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO2_ADDR);     
         pinconf2[OE_ADDR/4] &= pinconf2[OE_ADDR/4] &= (0xFFFFFFFF ^ ((1<<2)|(1 << 3)|(1<<5))); 
         //configure logic pins
         pinconf2[GPIO_DATAOUT/4]  |= (1 << 2); //set pin  P8_7
      	pinconf2[GPIO_DATAOUT/4]  &= ~(1 << 3); // clear pin P8_8
-        rt_task_set_periodic(NULL, TM_NOW, period2);
+        rt_task_set_periodic(NULL, TM_NOW, period_motor);
         rt_printf("Controling Left Motors!\n");
-        //previous = rt_timer_read();
+        
         while (1){
                 rt_task_wait_period(NULL);
                 previous = rt_timer_read();
                 pinconf2[GPIO_DATAOUT/4] |= (1 << 5); //PWM on pin P8_9
-                rt_task_sleep(duty2);
-                pinconf2[GPIO_DATAOUT/4] &= ~(1 << 5); //toggle pin
-  //              rt_printf("Left Motor PWM, time taken:%ld. %06ld ms\n", (long)(now-previous)/1000000, (long)(now-previous)%1000000); 
+                rt_task_sleep(pwm_l*period_motor);
+//		rt_task_sleep(dutyL);
+                pinconf2[GPIO_DATAOUT/4] &= ~(1 << 5); //toggle pin 
                 now = rt_timer_read();
-		if((long)((now - previous)%1000000) > MAX){
-                        MAX = (long)((now - previous)%1000000) ;
+                if (flag){
+                        MAX = now- previous;
+                        flag = 0;
+                }
+
+		if((long)((now - previous)) > MAX){
+                        MAX = (long)((now - previous)) ;
                 }
 //                rt_printf("WCET Left Motor: %ld \n", MAX);
 
-		//previous = now;
         }
 }
 
 
 void rEncoder(void *arg)
 {
-	int rEnc, lEnc, fd, rBias;
+	long rEnc, lEnc, fd, rBias;
         char buf[MAX_BUF];
-        char val[4]; //stores 4 digits ADC value
+        char val[15]; //stores 4 digits ADC value
         snprintf(buf, sizeof(buf), "/sys/devices/ocp.3/48302000.epwmss/48302180.eqep/position");
+
 	RTIME now, previous;
 	long MAX = 0;
-        rt_task_set_periodic(NULL, TM_NOW, period);
+	int flag = 1;
+
+        rt_task_set_periodic(NULL, TM_NOW, period_encoder);
         rt_printf("Reading right Encoder!\n");
 	
 	fd = open(buf, O_RDONLY); //Open ADC as read only
@@ -270,11 +344,11 @@ void rEncoder(void *arg)
                         perror("Problem opening right Encoder");
                 }
 
-         read(fd, &val, 4); //read upto 4 digits 0-1799
+         read(fd, &val, 15); //read upto 4 digits 0-1799
          close(fd);
 
          rBias = atoi(val); //return integer value
-
+	
 
         while (1){
                 rt_task_wait_period(NULL);
@@ -285,7 +359,7 @@ void rEncoder(void *arg)
                 	perror("Problem opening right Encoder");
         	}
 
-        	read(fd, &val, 4); //read upto 4 digits 0-1799
+        	read(fd, &val, 15); //read upto 4 digits 0-1799
         	close(fd);
 
         	rEnc = rBias - atoi(val); //return integer value
@@ -293,13 +367,19 @@ void rEncoder(void *arg)
 		rt_sem_p(&rSem, 0);
 		rtick = rEnc;
 		rt_sem_v(&rSem);
-        	rt_printf("right Encoder ticks: %d\n", rtick);
+		//rt_printf("bias right encoder: %lf\n", rEnc);
+        	rt_printf("right Encoder ticks: %li\n", rtick);
+
 		now = rt_timer_read();
-		if((long)((now - previous)%1000000) > MAX){
-			MAX = (long)((now - previous)%1000000) ;
+                if (flag){
+                        MAX = now- previous;
+                        flag = 0;
+                }
+
+		if((long)((now - previous)) > MAX){
+			MAX = (long)((now - previous)) ;
 		}
-		//rt_printf("WCET Right Motor: %ld \n", MAX);
-//		rt_queue_write(&rqueue, rEnc, sizeof(rEnc), Q_NORMAL);
+//		rt_printf("WCET Right Encoder: %ld \n", MAX);
 	}
 	return;
 }
@@ -307,13 +387,15 @@ void rEncoder(void *arg)
 
 void lEncoder(void *arg)
 {
-        int rEnc, lEnc, fd, lBias;
+        long rEnc, lEnc, fd, lBias;
         char buf[MAX_BUF];
-        char val[4]; //stores 4 digits ADC value
+        char val[15]; //stores 4 digits ADC value
         snprintf(buf, sizeof(buf), "/sys/devices/ocp.3/48304000.epwmss/48304180.eqep/position");
         RTIME now, previous;
         long MAX = 0;
-        rt_task_set_periodic(NULL, TM_NOW, period);
+	int flag = 1;
+
+        rt_task_set_periodic(NULL, TM_NOW, period_encoder);
         rt_printf("Reading Left Encoder!\n");
         fd = open(buf, O_RDONLY); //Open ADC as read only
 
@@ -321,7 +403,7 @@ void lEncoder(void *arg)
                         perror("Problem opening left Encoder");
                 }
 
-         read(fd, &val, 4); //read upto 4 digits 0-1799
+         read(fd, &val, 15); //read upto 4 digits 0-1799
          close(fd);
 
          lBias = atoi(val); //return integer value
@@ -335,22 +417,25 @@ void lEncoder(void *arg)
                         perror("Problem opening ADC");
                 }
 
-                read(fd, &val, 4); //read upto 4 digits 0-1799
+                read(fd, &val, 15); //read upto 4 digits 0-1799
                 close(fd);
 
                 lEnc = lBias - atoi(val); //return integer value
                 rt_sem_p(&lSem, 0);
                 ltick = lEnc;
                 rt_sem_v(&lSem);
-
-                rt_printf("Left Encoder ticks: %d\n", ltick);
+              //  rt_printf("bias left encoder: %d\n", lBias);
+                rt_printf("Left Encoder ticks: %li\n", ltick);
                 now = rt_timer_read();
-                if((long)((now - previous)%1000000) > MAX){
-                        MAX = (long)((now - previous)%1000000) ;
+                if (flag){
+                        MAX = now- previous;
+                        flag = 0;
                 }
-    //            rt_printf("WCET left Motor: %ld \n", MAX);
-        	//rt_queue_write(&lqueue, lEnc, sizeof(lEnc), Q_NORMAL);
 
+                if((long)((now - previous)) > MAX){
+                        MAX = (long)((now - previous)) ;
+                }
+//                rt_printf("WCET left Encoder: %ld \n", MAX);
         }
         return;
 } 
@@ -362,32 +447,27 @@ void Odo(void *arg){
 	double ltick_prev = 0;
 	double dtick_r = 0;
 	double dtick_l = 0;
-	char r_mesg[40], l_mesg[40];
 	int r_tick, l_tick;
 
-        double Dr, Dc, Dl, x_dt, y_dt, theta_dt, x_new, y_new, theta_new;
+	int flag = 1;
+        RTIME now, previous;
+        long MAX = 0;
 
+        double Dr, Dc, Dl, x_dt, y_dt, theta_dt, x_new, y_new, theta_new;
 	Dr = Dc = Dl = 0;
 	x = y = theta = 0;
 	x_new=y_new=theta_new = 0;
 	x_dt = y_dt = theta_dt = 0;
 
-	rt_task_set_periodic(NULL, TM_NOW, period);	
+	rt_task_set_periodic(NULL, TM_NOW, period_odo);	
 
 	while(1){
 		rt_task_wait_period(NULL);
-	
-//		rt_queue_read(&rqueue, r_mesg, sizeof(r_mesg), TM_INFINITE);
-//		rt_queue_read(&lqueue, l_mesg, sizeof(l_mesg), TM_INFINITE);
-
-//		r_tick = atoi(r_mesg);
-//		l_tick = atoi(l_mesg);
+		previous = rt_timer_read();
 
 		r_tick = rtick;
 		l_tick = ltick;
-		//rt_printf("int data: %d \n", l_tick);
-		//rt_printf("data r : %d \n", r_tick);
-
+		
 		dtick_r = r_tick - rtick_prev;
                 dtick_l = l_tick - ltick_prev;
 
@@ -418,6 +498,16 @@ void Odo(void *arg){
 
 		rt_printf("Robot pose (x, y, theta) is: %lf, %lf, %lf\n", x, y, theta);
 		
+	        now = rt_timer_read();
+                if (flag){
+                        MAX = now- previous;
+                        flag = 0;
+                }
+
+                if((long)((now - previous)) > MAX){
+                        MAX = (long)((now - previous)) ;
+                }
+//                rt_printf("WCET Odometry: %ld \n", MAX);
 
 	}
 
@@ -430,9 +520,6 @@ void init_xenomai(){
 
 void startup(){
 
-	//rt_queue_create(&rqueue, "rQueue", QUEUE_SIZE, 40, Q_FIFO);
-        //rt_queue_create(&lqueue, "lQueue", QUEUE_SIZE, 40, Q_FIFO);
-	
 	rt_sem_create(&rSem, "rSem", 1, S_FIFO);
         rt_sem_create(&lSem, "lSem", 1, S_FIFO);
 	rt_sem_create(&xSem, "xSem", 1, S_FIFO);
@@ -440,37 +527,24 @@ void startup(){
         rt_sem_create(&tSem, "tSem", 1, S_FIFO);
         rt_sem_create(&rPwm, "rPwm", 1, S_FIFO);
         rt_sem_create(&lPwm, "lPwm", 1, S_FIFO);
-
-        rt_task_create(&rEncoder_task, "rEnc Task", 0, 50, 0);
-        rt_task_start(&rEncoder_task, &rEncoder, 0);
-
        
-        rt_task_create(&lEncoder_task, "lEnc Task", 0, 50, 0);
-      	rt_task_start(&lEncoder_task, &lEncoder, 0);
- 
+	rt_task_create(&testGoal_task, "check Goal", 0, 90, 0);
+        rt_task_create(&rMotor_task, "rMotor", 0, 70, 0);
+        rt_task_create(&lMotor_task, "lMotor", 0, 70, 0);
+        rt_task_create(&stop_lMotor, "StoplMotor", 0, 90, 0);
+        rt_task_create(&stop_rMotor, "StoprMotor", 0, 90, 0);
+        rt_task_create(&rEncoder_task, "rEnc Task", 0, 51, 0);
+        rt_task_create(&lEncoder_task, "lEnc Task", 0, 51, 0);
         rt_task_create(&Odo_task, "Odo Task", 0, 60, 0);
+        rt_task_create(&gtg_task, "GTG Task", 0, 61, 0);
+
+	rt_task_start(&testGoal_task, &testGoal, 0);
+        rt_task_start(&rMotor_task, &rMotor, 0);
+        rt_task_start(&lMotor_task, &lMotor, 0);
+        rt_task_start(&rEncoder_task, &rEncoder, 0);
+      	rt_task_start(&lEncoder_task, &lEncoder, 0);
         rt_task_start(&Odo_task, &Odo, 0);
- 
-        rt_task_create(&gtg_task, "GTG Task", 0, 70, 0);
         rt_task_start(&gtg_task, &gtg, 0);
-
-    	char str[10];
-	sprintf(str, "rMotor");
-	//rt_task_create(&rMotor_task, str, 0, 50, 0);
-	//rt_task_start(&rMotor_task, &rMotor, 0);
-
-        sprintf(str, "lMotor"); 
-        //rt_task_create(&lMotor_task, str, 0, 50, 0);
-        //rt_task_start(&lMotor_task, &lMotor, 0);
-
-        sprintf(str, "StoplMotor"); 
-        rt_task_create(&stop_lMotor, str, 0, 90, 0);
-
-        sprintf(str, "StoprMotor"); 
-        rt_task_create(&stop_rMotor, str, 0, 90, 0);
-
-	//rt_sem_broadcast(&rsem);
-
 }
 
 void catch_signal(int sig){
@@ -497,10 +571,7 @@ void cleanup(){
         rt_task_delete(&rMotor_task);
 	rt_task_delete(&stop_lMotor);
 	rt_task_delete(&stop_rMotor);
-
-//	rt_task_delete(&rEncoder_task);
-//        rt_task_delete(&lEncoder_task);
-//	rt_task_delete(&Odo_task);
+	rt_task_delete(&testGoal_task);
 
 }
 
@@ -515,12 +586,12 @@ int main(int argc, char* argv[])
 	init_xenomai();
 
 	startup();
-	begin = time(NULL);
-	while(timeD < 3){
+/*	begin = time(NULL);
+	while(timeD < 10){
 		end = time(NULL);
 		timeD = (end - begin);	
-	}
-	//wait_for_ctrl_c();
+	}*/
+	wait_for_ctrl_c();
 
 	cleanup();
 
