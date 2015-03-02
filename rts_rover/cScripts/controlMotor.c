ae1cfbe0270e4c1951bb0ad86d240c19e7b28136
root <root@beaglebone.localdomain>
2015-03-02 01:10:47 +0000

    added c script to drive motors on land rover 5 - controlMotor.c

---
+++ rts_rover/cScripts/controlMotor.c
@@ -0,0 +1,53 @@
+/* Control a DC motor:
+Motor 1 & 3: Right 
+Logic 1 - P9_12 - GPIO1[28]
+Logic 2 - P9_15 - GPIO1[16]
+PWM 1 - P9_23 - GPIO1[17]
+
+Motor 2 & 4: Left
+Logic 1 - P8_7 - GPIO2[2]
+Logic 2 - P8_8 - GPIO2[3]
+PWM 1 - P8_9 - GPIO2[5]
+
+*/
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+//#include <iostream>
+//#include<stdio.h>
+#define OE_ADDR 0x134
+#define GPIO_DATAOUT 0x13C
+#define GPIO_DATAIN 0x138
+#define GPIO0_ADDR 0x44E07000
+#define GPIO1_ADDR 0x4804C000
+#define GPIO2_ADDR 0x481AC000
+#define GPIO3_ADDR 0x481AF000
+//using namespace std;
+
+int main(){
+    int i = 0;
+    int fd = open("/dev/mem",O_RDWR | O_SYNC);
+    ulong* pinconf1 =  (ulong*) mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO1_ADDR);
+    ulong* pinconf2 =  (ulong*) mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO2_ADDR);
+    //configure motor pins as output
+    pinconf1[OE_ADDR/4] &=  pinconf1[OE_ADDR/4] &= (0xFFFFFFFF ^ ((1 << 28)|(1<<16)|(1<<17)|(1<<5)|(1<<4)|(1<<1)));    
+    pinconf2[OE_ADDR/4] &=  pinconf2[OE_ADDR/4] &= (0xFFFFFFFF ^ ((1<<2)|(1 << 3)|(1<<5))); 
+    
+   //configure logic pins
+     pinconf1[GPIO_DATAOUT/4]  |= (1 << 28); //set pin  P9_12
+     pinconf1[GPIO_DATAOUT/4]  &= ~(1 << 17); // clear pin P9_15
+     pinconf2[GPIO_DATAOUT/4]  |= (1 << 2); //set pin  P8_7
+     pinconf2[GPIO_DATAOUT/4]  &= ~(1 << 3); // clear pin P8_8
+
+   //generate PWM
+for(i = 0 ; i < 5; i++){
+     pinconf1[GPIO_DATAOUT/4]  |= (1 << 17); //PWM on pin P9_23
+     pinconf2[GPIO_DATAOUT/4]  |= (1 << 5); //PWM on pin P8_9
+     sleep(1);
+     pinconf1[GPIO_DATAOUT/4]  ^= (1 << 17); //toggle pin
+     pinconf2[GPIO_DATAOUT/4]  ^= (1 << 5); //toggle pin
+     sleep(1);
+    }
+    return 0;
+}
---
+++ rts_rover/rtScripts/ex03.c
@@ -0,0 +1,139 @@
+/*
+ This exampe shows how to launch multiple tasks  where some tasks are periodic while other
+are one-shot.
+*/
+
+// Shriya Shah, 20th April, 2016
+ 
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <native/task.h>
+#include <native/timer.h>
+#include <rtdk.h>
+#include <signal.h>
+
+#define period1 1000000000
+#define period2 2000000000
+#define period3 3000000000
+
+RT_TASK demo_task1;
+RT_TASK demo_task2;
+RT_TASK demo_task3;
+RT_TASK demo_task4;
+RT_TASK demo_task5;
+
+void demo1(void *arg)
+{
+	RT_TASK *curtask;
+	RT_TASK_INFO curtaskinfo;
+       // int num =  (int *)arg;
+
+	rt_printf("In Task 1!\n");
+	curtask = rt_task_self();
+	rt_task_inquire(curtask, &curtaskinfo);
+	rt_printf("Task name: %s \n", curtaskinfo.name);
+	//rt_printf("argument passed is: %d\n", *arg);
+
+}
+void demo2(void *arg)
+{
+        RT_TASK *curtask;
+        RT_TASK_INFO curtaskinfo;
+	//int num =  (int *)arg;
+
+        rt_printf("In Task 2!\n");
+        curtask = rt_task_self();
+        rt_task_inquire(curtask, &curtaskinfo);
+        rt_printf("Task name: %s \n", curtaskinfo.name);
+	//rt_printf("argument passed is: %d\n", *arg);
+}
+
+//Periodic task
+void demo3(void *arg)
+{
+        RT_TASK *curtask;
+        RT_TASK_INFO curtaskinfo;
+	RTIME now, previous;	
+	rt_task_set_periodic(NULL, TM_NOW, period1);
+        rt_printf("In Task 3!\n");
+	previous = rt_timer_read();
+	while (1){
+                rt_task_wait_period(NULL);
+		now = rt_timer_read();
+		rt_printf("in periodic task 3, time taken:%ld. %06ld ms\n", (long)(now-previous)/1000000, (long)(now-previous)%1000000);
+		previous = now;
+	}
+}
+void demo4(void *arg)
+{
+        RT_TASK *curtask;
+        RT_TASK_INFO curtaskinfo;
+	RTIME now, previous;       
+        rt_printf("In Task 4!\n");
+        rt_task_set_periodic(NULL, TM_NOW, period2);
+ 	previous = rt_timer_read();
+
+        while (1){
+                rt_task_wait_period(NULL);
+                now = rt_timer_read();
+                rt_printf("in periodic task 4, time taken:%ld. %06ld ms\n", (long)(now-previous)/1000000, (long)(now-previous)%1000000);
+                previous = now;
+
+        }
+}
+
+void demo5(void *arg)
+{
+        RT_TASK *curtask;
+        RT_TASK_INFO curtaskinfo;
+	RTIME now, previous;
+        rt_task_set_periodic(NULL, TM_NOW, period3);
+        rt_printf("In Task 5!\n");
+	previous = rt_timer_read();
+        while (1){
+                rt_task_wait_period(NULL);
+                now = rt_timer_read();
+                rt_printf("in periodic task 5, time taken:%ld. %06ld ms\n", (long)(now-previous)/1000000, (long)(now-previous)%1000000);
+                previous = now;
+
+        }
+
+}
+
+
+int main(int argc, char* argv[])
+{
+	char str[10];
+	int i = 1;
+	int j = 2;
+	rt_print_auto_init(1);
+	mlockall(MCL_CURRENT|MCL_FUTURE);
+	rt_printf("start task\n");
+	
+	sprintf(str, "task 1");
+	//&task, name, stack size (0 - default), priority, mode 
+	rt_task_create(&demo_task1, str, 0, 50, 0);
+	//&task, task function, function argument
+	rt_task_start(&demo_task1, &demo1, 0);
+
+        sprintf(str, "task 2");
+        rt_task_create(&demo_task2, str, 0,50, 0);
+        rt_task_start(&demo_task2, &demo2, 0);
+
+        sprintf(str, "task 3");
+        rt_task_create(&demo_task3, str, 0, 50, 0);
+        rt_task_start(&demo_task3, &demo3, 0);
+
+        sprintf(str, "task 4");
+        rt_task_create(&demo_task4, str, 0, 50, 0);
+        rt_task_start(&demo_task4, &demo4, 0);
+
+        sprintf(str, "task 5");
+        rt_task_create(&demo_task5, str, 0, 50, 0);
+        rt_task_start(&demo_task5, &demo5, 0);
+
+	rt_printf("end program : ctrl+c\n");
+	pause();
+
+}
